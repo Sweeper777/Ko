02279fa7a17fd7780fcb6cac9cbcb1ccabb44790
Mulang Su <sumulang@gmail.com>
2021-09-01 11:37:23 +0800

    Initial Commit

---
+++ Ko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ko
+//
+//  Created by Mulang Su on 1/9/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
