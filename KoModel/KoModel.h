03b5fd2a7e9e1c8f95ec87680b6a4f7b99c97245
Mulang Su <sumulang@gmail.com>
2021-09-02 11:07:18 +0800

    added model and test targets

---
+++ KoModel/KoModel.h
@@ -0,0 +1,18 @@
+//
+//  KoModel.h
+//  KoModel
+//
+//  Created by Mulang Su on 1/9/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KoModel.
+FOUNDATION_EXPORT double KoModelVersionNumber;
+
+//! Project version string for KoModel.
+FOUNDATION_EXPORT const unsigned char KoModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KoModel/PublicHeader.h>
+
+
---
+++ KoModelMacOS/KoModelMacOS.h
@@ -0,0 +1,18 @@
+//
+//  KoModelMacOS.h
+//  KoModelMacOS
+//
+//  Created by Mulang Su on 1/9/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KoModelMacOS.
+FOUNDATION_EXPORT double KoModelMacOSVersionNumber;
+
+//! Project version string for KoModelMacOS.
+FOUNDATION_EXPORT const unsigned char KoModelMacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KoModelMacOS/PublicHeader.h>
+
+
