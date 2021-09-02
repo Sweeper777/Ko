656b5a0dcb699cb7b418dbb68904c7cd950128fa
Mulang Su <sumulang@gmail.com>
2021-09-02 11:07:32 +0800

    Create GameConstants.swift

---
+++ KoModel/GameConstants.swift
@@ -0,0 +1,7 @@
+public enum GameConstants {
+    public static let initialFields = 111
+    public static let initialBurrows = 3
+    public static let initialRabbits = 1
+    public static let initialHares = 1
+    public static let initialMoons = 1
+}
