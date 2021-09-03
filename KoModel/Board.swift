a1617450620fc989ef0ba9255877df5d2ed4f4f4
Mulang Su <sumulang@gmail.com>
2021-09-03 11:19:02 +0800

    added isExactlyOneGrassland algorithm

--- KoModel/Board.swift
+++ KoModel/Board.swift
@@ -13,4 +13,24 @@ public class Board {
         board[position]
     }
     
+    
+    func isExactlyOneGrassland(startPosition start: Position) -> Bool {
+        var unreached = piecesPositions.values.reduce(Set()) { $0.union($1) }
+        unreached.remove(start)
+        var queue = [start]
+        while !queue.isEmpty {
+            let pos = queue.removeFirst()
+            let neighbours = [
+                pos.above(), pos.below(), pos.left(), pos.right(),
+                pos.above().left(), pos.above().right(),
+                pos.below().left(), pos.below().right()
+            ].filter { board[safe: $0]?.isEmpty == false }
+            for neighbour in neighbours {
+                if let removed = unreached.remove(neighbour) {
+                    queue.append(removed)
+                }
+            }
+        }
+        return unreached.isEmpty
+    }
 }
