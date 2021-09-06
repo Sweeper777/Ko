567198c0f3a094b8a923fad842fc7dffdd884f8f
Mulang Su <sumulang@gmail.com>
2021-09-06 10:21:10 +0800

    added a convenient Rule class

--- KoModel/Rule.swift
+++ KoModel/Rule.swift
@@ -7,3 +7,23 @@ protocol RuleProtocol {
     func apply(to game: Game, move: Move, pendingMoveResult: inout MoveResult) -> RuleApplicationResult
 }
 
+class Rule : RuleProtocol {
+    
+    private let isApplicableFunc: (Game, Move) -> Bool
+    private let applyFunc: (Game, Move, inout MoveResult) -> RuleApplicationResult
+    
+    init(isApplicable: @escaping (Game, Move) -> Bool, apply: @escaping (Game, Move, inout MoveResult) -> RuleApplicationResult) {
+        isApplicableFunc = isApplicable
+        applyFunc = apply
+    }
+    
+    
+    func isApplicable(to game: Game, move: Move) -> Bool {
+        isApplicableFunc(game, move)
+    }
+    
+    func apply(to game: Game, move: Move, pendingMoveResult: inout MoveResult) -> RuleApplicationResult {
+        applyFunc(game, move, &pendingMoveResult)
+    }
+}
+
