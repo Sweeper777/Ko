28d91f3d6366de56904014f12f8b990ad23dcda0
Mulang Su <sumulang@gmail.com>
2021-09-07 11:08:08 +0800

    added IfViolatedApplyRule

--- KoModel/Rule.swift
+++ KoModel/Rule.swift
@@ -31,3 +31,36 @@ class Rule : RuleProtocol, CustomStringConvertible {
     }
 }
 
+class IfViolatedApplyRule: RuleProtocol, CustomStringConvertible {
+    
+    private let rule1: RuleProtocol
+    private let rule2: RuleProtocol
+    
+    fileprivate init(rule1: RuleProtocol, rule2: RuleProtocol) {
+        self.rule1 = rule1
+        self.rule2 = rule2
+    }
+    
+    func isApplicable(to game: Game, move: Move) -> Bool {
+        rule1.isApplicable(to: game, move: move)
+    }
+    
+    func apply(to game: Game, move: Move, pendingMoveResult: inout MoveResult) -> RuleApplicationResult {
+        let rule1Result = rule1.apply(to: game, move: move, pendingMoveResult: &pendingMoveResult)
+        if rule1Result == .violation {
+            return rule2.apply(to: game, move: move, pendingMoveResult: &pendingMoveResult)
+        } else {
+            return rule1Result
+        }
+    }
+    
+    var description: String {
+        "if \"\(rule1)\" is violated, apply \"\(rule2)\""
+    }
+}
+
+extension RuleProtocol {
+    func ifViolatedApply(_ rule: RuleProtocol) -> RuleProtocol {
+        IfViolatedApplyRule(rule1: self, rule2: rule)
+    }
+}
