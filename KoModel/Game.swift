441aa2e83361d680a11ff56c8590d9ae6760b0da
Mulang Su <sumulang@gmail.com>
2021-09-08 09:07:52 +0800

    implemented makeMove

--- KoModel/Game.swift
+++ KoModel/Game.swift
@@ -4,6 +4,9 @@ public class Game {
     public private(set) var currentTurn = PlayerColor.blue
     public private(set) var board = Board()
     public private(set) var currentTurnNumber = 0
+    public private(set) var result = GameResult.notDetermined
+    
+    private let rules = allRules
     
     public var currentPlayer: Player {
         playerOfColor(currentTurn)
@@ -20,7 +23,45 @@ public class Game {
         }
     }
     
+    public func makeMove(_ move: Move) -> MoveResult? {
+        var moveResult = MoveResult()
+        var hasApplicableRules = false
+        for rule in rules {
+            if rule.isApplicable(to: self, move: move) {
+                hasApplicableRules = true
+                let ruleApplicationResult = rule.apply(to: self, move: move, pendingMoveResult: &moveResult)
+                if ruleApplicationResult == .violation {
+                    return nil
+                }
+            }
         }
+        guard hasApplicableRules else {
+            return nil
+        }
+        for removedPieceRecord in moveResult.piecesRemoved.sorted(by: { $0.stackIndex > $1.stackIndex }) {
+            let removedPiece = board.removePiece(at: removedPieceRecord.position, stackIndex: removedPieceRecord.stackIndex)
+            resolveRemovedPiece(removedPiece)
+        }
+        if moveResult.hasCapture, let toPosition = moveResult.toPosition,
+           let removedPiece = board.removePiece(at: toPosition) {
+            resolveRemovedPiece(removedPiece)
+        }
+        var movedPiece: Piece?
+        if let fromPosition = moveResult.fromPosition {
+            movedPiece = board.removePiece(at: fromPosition)
+        }
+        board.conquerFields(positions: moveResult.conqueredPositions)
+        if let movedPiece = movedPiece, let toPosition = moveResult.toPosition {
+            board.placePiece(movedPiece, at: toPosition)
+        }
+        if let placedPieceRecord = moveResult.piecePlaced {
+            board.placePiece(Piece(currentTurn, placedPieceRecord.pieceType), at: placedPieceRecord.position)
+        }
+        result = moveResult.gameResult
+        if case .notDetermined = result {
+            nextTurn()
+        }
+        return moveResult
     }
     
     
