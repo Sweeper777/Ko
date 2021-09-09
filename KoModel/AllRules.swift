7f51759f6656650284cbddcc4295751385d0346d
Mulang Su <sumulang@gmail.com>
2021-09-09 10:38:15 +0800

    added rules that set fromPosition, toPosition and piecePlaced

--- KoModel/AllRules.swift
+++ KoModel/AllRules.swift
@@ -1,13 +1,24 @@
 let allRules: [RuleProtocol] = [
-    Rule("All moves must be in range of the board", apply: { game, move, _ in
-            func inRange(_ position: Position) -> Bool {
-                game.board.board[safe: position] != nil
-            }
-            switch move {
-            case .move(from: let pos1, to: let pos2):
-                return inRange(pos1) && inRange(pos2) ? .compliance : .violation
-            case .placePiece(at: let pos):
-                return inRange(pos) ? .compliance : .violation
+    Rule("all moves must be in range of the board", apply: { game, move, _ in
+        func inRange(_ position: Position) -> Bool {
+            game.board.board[safe: position] != nil
+        }
+        switch move {
+        case .move(from: let pos1, to: let pos2):
+            return inRange(pos1) && inRange(pos2) ? .compliance : .violation
+        case .placePiece(_, at: let pos):
+            return inRange(pos) ? .compliance : .violation
+        }
+    }),
+    MoveRule("moves can move pieces", apply: { _, from, to, result in
+        result.fromPosition = from
+        result.toPosition = to
+        return .compliance
+    }),
+    PlacePieceRule("moves can place pieces", apply: { _, piecePlaced, result in
+        result.piecePlaced = piecePlaced
+        return .compliance
+    }),
             }
     }),
 ]
