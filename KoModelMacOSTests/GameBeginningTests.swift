a3a48fd789175aa59b52ce5e4ac00902e6924ef9
Mulang Su <sumulang@gmail.com>
2021-09-10 16:31:09 +0800

    tested some invalid beginnings of the game

--- KoModelMacOSTests/GameBeginningTests.swift
+++ KoModelMacOSTests/GameBeginningTests.swift
@@ -66,5 +66,60 @@ class GameBeginningTests: XCTestCase {
             Piece(.white, .empress): [.init(11,6)],
         ])
     }
+    
+    func testInvalidGameBeginnings() {
+        let game = Game()
+        // try placing fields not in the center
+        XCTAssertNil(game.makeMove(.placePiece(.field, at: .init(0, 0))))
+        XCTAssertEqual(game.currentTurn, .blue)
+        
+        // try moving a non-existent piece
+        XCTAssertNil(game.makeMove(.move(from: .init(9, 9), to: .init(0, 0))))
+        
+        // try placing a non-field
+        XCTAssertNil(game.makeMove(.placePiece(.empress, at: .init(9, 9))))
+        
+        XCTAssertNotNil(game.makeMove(.placePiece(.field, at: .init(9, 9))))
+        XCTAssertNotNil(game.makeMove(.placePiece(.field, at: .init(10, 8))))
+        
+        // try placing a field on the opponent's side
+        XCTAssertNil(game.makeMove(.placePiece(.field, at: .init(9, 8))))
+        
+        // try placing a field that is only connected on the edge
+        XCTAssertNil(game.makeMove(.placePiece(.field, at: .init(8, 10))))
+        
+        XCTAssertNotNil(game.makeMove(.placePiece(.field, at: .init(10, 9))))
+        XCTAssertNotNil(game.makeMove(.placePiece(.field, at: .init(9, 8))))
+        XCTAssertNotNil(game.makeMove(.placePiece(.field, at: .init(8, 9))))
+        XCTAssertNotNil(game.makeMove(.placePiece(.field, at: .init(8, 8))))
+        
+        // try placing a non-field again
+        XCTAssertNil(game.makeMove(.placePiece(.empress, at: .init(9, 9))))
+        
+        for x in 7...11 {
+            for y in 7...10 {
+                if (x, y) == (8, 10) || (x, y) == (10, 10) {
+                    continue
+                }
+                XCTAssertNil(game.makeMove(.placePiece(.field, at: .init(x, y))))
+            }
+        }
+        XCTAssertNotNil(game.makeMove(.placePiece(.field, at: .init(10, 10))))
+        XCTAssertNotNil(game.makeMove(.placePiece(.field, at: .init(10, 7))))
+        
+        // try placing a fifth field
+        XCTAssertNil(game.makeMove(.placePiece(.field, at: .init(10, 11))))
+        
+        // try placing a disconnected empress
+        XCTAssertNil(game.makeMove(.placePiece(.empress, at: .init(6, 9))))
+        
+        // try placing a empress disconnected to own pieces
+        XCTAssertNil(game.makeMove(.placePiece(.empress, at: .init(8, 7))))
+        
+        XCTAssertNotNil(game.makeMove(.placePiece(.empress, at: .init(7, 9))))
+        
+        // try placing 2 empresses close together
+        XCTAssertNil(game.makeMove(.placePiece(.empress, at: .init(7, 8))))
+    }
 
 }
