a109822800083f4c3438e829eef23440b00b69ec
Mulang Su <sumulang@gmail.com>
2021-09-10 16:29:21 +0800

    completed the existence rules for expresses

--- KoModel/AllRules.swift
+++ KoModel/AllRules.swift
@@ -121,6 +121,14 @@ let allRules: [RuleProtocol] = [
             return .violation
         }
     },
+    ExistenceRule("there must not an opponent moon in the 4-neighbourhood of an empress", for: .empress, apply: {
+        game, position, _ in
+        if position.fourNeighbours.allSatisfy({ game.board.board[safe: $0]?.top != Piece(game.currentTurn.opposingColor, .moon) }) {
+            return .compliance
+        } else {
+            return .violation
+        }
+    }),
     Rule("after every move, the grassland must be connected", apply: {
         game, move, result in
         
