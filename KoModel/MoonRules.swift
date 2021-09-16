dce488137224d79f7d85aebd16e0b3bcb0ea1c6f
Mulang Su <sumulang@gmail.com>
2021-09-16 22:23:44 +0800

    added MoonCaptureRule

--- KoModel/MoonRules.swift
+++ KoModel/MoonRules.swift
@@ -14,3 +14,15 @@ class MoonMovementRule: MoveRule {
     }
 }
 
+class MoonCaptureRule: MoveRule {
+    init() {
+        super.init("moons can capture fields, rabbits and hares", for: .moon, isApplicable: {
+            game, _, to in [PieceType.field, .rabbit, .hare].contains(game.board[to].top?.type)
+        }) {
+            _, _, _, result in
+            result.hasCapture = true
+            return .compliance
+        }
+    }
+}
+
