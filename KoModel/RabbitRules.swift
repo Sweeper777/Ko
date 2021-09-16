c8e3e274447646a555b9ec08f6ded05c141a06c2
Mulang Su <sumulang@gmail.com>
2021-09-16 22:24:31 +0800

    added RabbitJumpRule

--- KoModel/RabbitRules.swift
+++ KoModel/RabbitRules.swift
@@ -14,3 +14,46 @@ class RabbitMovementRule: MoveRule {
     }
 }
 
+class RabbitJumpRule: MoveRule {
+    init() {
+        super.init("rabbits can jump horizontally, vertically or diagonally to an empty square, given that all the squares along the path are occupied", for: .rabbit) {
+            game, from, to, _ in
+            guard game.board[to].isEmpty else {
+                return .violation
+            }
+            guard from != to else {
+                return .violation
+            }
+            let deltaX = to.x - from.x
+            let deltaY = to.y - from.y
+                    // horizontal/vertical
+            guard ((deltaX == 0) != (deltaY == 0)) ||
+                    // diagonal
+                    abs(deltaX) == abs(deltaY) else {
+                return .violation
+            }
+            
+            let xs: [Int]
+            let ys: [Int]
+            if deltaX != 0 { // horizontal
+                xs = Array(stride(from: from.x + deltaX.signum(), to: to.x, by: deltaX.signum()))
+                ys = Array(repeating: from.y, count: xs.count)
+                
+            } else if deltaY != 0 { // vertical
+                ys = Array(stride(from: from.y + deltaY.signum(), to: to.y, by: deltaY.signum()))
+                xs = Array(repeating: from.x, count: ys.count)
+            } else { // diagonal
+                xs = Array(stride(from: from.x + deltaX.signum(), to: to.x, by: deltaX.signum()))
+                ys = Array(stride(from: from.y + deltaY.signum(), to: to.y, by: deltaY.signum()))
+            }
+            for (x, y) in zip(xs, ys) {
+                let pos = Position(x, y)
+                if game.board[pos].isEmpty {
+                    return .violation
+                }
+            }
+            return .compliance
+        }
+    }
+}
+
