3aed5358d66cbcbecf13f8016d24a9d8c187e7ab
Mulang Su <sumulang@gmail.com>
2021-09-17 10:27:24 +0800

    added the thresholds for the placeable pieces into GameConstants

--- KoModel/GameConstants.swift
+++ KoModel/GameConstants.swift
@@ -6,4 +6,10 @@ public enum GameConstants {
     public static let initialMoons = 1
     public static let boardColumns = 19
     public static let boardRows = 18
+    public static let firstBurrowThreshold = 8
+    public static let secondBurrowThreshold = 22
+    public static let thirdBurrowThreshold = 36
+    public static let firstRabbitHareThreshold = 14
+    public static let secondRabbitHareThreshold = 28
+    public static let moonThreshold = 43
 }
