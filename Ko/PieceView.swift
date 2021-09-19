762bdfb651887ceb603036c235da289dd364a151
Mulang Su <sumulang@gmail.com>
2021-09-19 13:12:04 +0800

    added PieceView

---
+++ Ko/PieceView.swift
@@ -0,0 +1,19 @@
+import UIKit
+import KoModel
+
+class PieceView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        backgroundColor = .clear
+        clipsToBounds = false
+    }
+    
+}
