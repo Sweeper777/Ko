770c56d6c5d563a00660c2d262d8a60557a0f955
Mulang Su <sumulang@gmail.com>
2021-09-19 13:12:29 +0800

    drew the basic piece shape

--- Ko/PieceView.swift
+++ Ko/PieceView.swift
@@ -16,4 +16,38 @@ class PieceView: UIView {
         clipsToBounds = false
     }
     
+    var pieces: PieceStack = .init(){
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let piece = pieces.top else {
+            return
+        }
+        
+        let topProportion: CGFloat = 0.2
+        let bottomProportion: CGFloat = 0.6
+        let bottomHeightProportion: CGFloat = 0.4
+        let fontSize: CGFloat = 16
+        
+        let lineWidth: CGFloat = 4
+        let drawingRect = bounds.insetBy(dx: bounds.width * 0.1, dy: bounds.height * 0.1)
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: drawingRect.minX + drawingRect.width * (1 - topProportion) / 2,
+                              y: drawingRect.minY))
+        path.addLine(to: CGPoint(x: drawingRect.minX,
+                                 y: drawingRect.minY + drawingRect.height * (1 - bottomHeightProportion)))
+        path.addLine(to: CGPoint(x: drawingRect.minX + drawingRect.width * (1 - bottomProportion) / 2,
+                                 y: drawingRect.maxY))
+        path.addLine(to: CGPoint(x: drawingRect.maxX - drawingRect.width * (1 - bottomProportion) / 2,
+                                 y: drawingRect.maxY))
+        path.addLine(to: CGPoint(x: drawingRect.maxX,
+                                 y: drawingRect.minY + drawingRect.height * (1 - bottomHeightProportion)))
+        path.addLine(to: CGPoint(x: drawingRect.maxX - drawingRect.width * (1 - topProportion) / 2,
+                              y: drawingRect.minY))
+        path.close()
+        path.lineWidth = lineWidth
+    }
 }
