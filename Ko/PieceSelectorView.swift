90e923f46970a29201d103468de2b6ee6a85e91e
Mulang Su <sumulang@gmail.com>
2021-09-23 12:27:00 +0800

    Create PieceSelectorView.swift

---
+++ Ko/PieceSelectorView.swift
@@ -0,0 +1,20 @@
+import UIKit
+import KoModel
+
+class PieceSelectorView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+    }
+    
+}
+
