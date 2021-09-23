062594e93f59a392827400703475879f35074c32
Mulang Su <sumulang@gmail.com>
2021-09-23 12:27:30 +0800

    implemented commonInit for PieceSelectorView

--- Ko/PieceSelectorView.swift
+++ Ko/PieceSelectorView.swift
@@ -2,6 +2,19 @@ import UIKit
 import KoModel
 
 class PieceSelectorView: UIView {
+    var selectablePieces: [Piece] = [] {
+        didSet {
+            updatePieceViews()
+        }
+    }
+    var selectedPiece: Piece? {
+        didSet {
+            delegate?.pieceSelectorValueDidChange(self, selectedPiece: selectedPiece)
+            updatePieceViews()
+        }
+    }
+    
+    private var stackView: UIStackView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -14,6 +27,18 @@ class PieceSelectorView: UIView {
     }
     
     private func commonInit() {
+        stackView = UIStackView(frame: bounds)
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .equalCentering
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.leftAnchor.constraint(equalTo: leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: rightAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
     }
     
 }
