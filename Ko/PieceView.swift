2063be6d03c8219643d8542f6a78e41fcc85f244
Mulang Su <sumulang@gmail.com>
2021-09-27 10:44:24 +0100

    drew the piece shapes with CAShapeLayer

--- Ko/PieceView.swift
+++ Ko/PieceView.swift
@@ -19,13 +19,13 @@ class PieceView: UIView {
     
     var pieces: PieceStack = .init(){
         didSet {
-            setNeedsDisplay()
+            setupLayers()
         }
     }
     
     var isSelected: Bool = false {
         didSet {
-            setNeedsDisplay()
+            setNeedsLayout()
         }
     }
     
@@ -46,7 +46,7 @@ class PieceView: UIView {
         setNeedsLayout()
     }
     
-    private func drawPiece(_ piece: Piece) {
+    override func layoutSubviews() {
         let topProportion: CGFloat = 0.2
         let bottomProportion: CGFloat = 0.6
         let bottomHeightProportion: CGFloat = 0.4
@@ -54,66 +54,38 @@ class PieceView: UIView {
         
         let lineWidth: CGFloat = 4
         let drawingRect = bounds.insetBy(dx: bounds.width * 0.1, dy: bounds.height * 0.1)
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: drawingRect.minX + drawingRect.width * (1 - topProportion) / 2,
-                              y: drawingRect.minY))
-        path.addLine(to: CGPoint(x: drawingRect.minX,
-                                 y: drawingRect.minY + drawingRect.height * (1 - bottomHeightProportion)))
-        path.addLine(to: CGPoint(x: drawingRect.minX + drawingRect.width * (1 - bottomProportion) / 2,
-                                 y: drawingRect.maxY))
-        path.addLine(to: CGPoint(x: drawingRect.maxX - drawingRect.width * (1 - bottomProportion) / 2,
-                                 y: drawingRect.maxY))
-        path.addLine(to: CGPoint(x: drawingRect.maxX,
-                                 y: drawingRect.minY + drawingRect.height * (1 - bottomHeightProportion)))
-        path.addLine(to: CGPoint(x: drawingRect.maxX - drawingRect.width * (1 - topProportion) / 2,
-                              y: drawingRect.minY))
-        path.close()
-        path.lineWidth = lineWidth
-        let textColor: UIColor
-        let textOffset: CGFloat
-        switch piece.color {
-        case .blue:
-            UIColor(named: "bluePlayerColor")?.setFill()
-            UIColor(named: "bluePlayerColor")?.lighter().setStroke()
-            textColor = .white
-            textOffset = 5
-        case .white:
-            UIColor(named: "whitePlayerColor")?.setFill()
-            UIColor(named: "whitePlayerColor")?.darker().setStroke()
-            textColor = .black
-            textOffset = -5
-            path.apply(CGAffineTransform(scaleX: 1, y: -1))
-            path.apply(CGAffineTransform(translationX: 0, y: bounds.height))
+        for (piece, layer) in zip(pieces, pieceLayers) {
+            let path = UIBezierPath()
+            path.move(to: CGPoint(x: drawingRect.minX + drawingRect.width * (1 - topProportion) / 2,
+                                  y: drawingRect.minY))
+            path.addLine(to: CGPoint(x: drawingRect.minX,
+                                     y: drawingRect.minY + drawingRect.height * (1 - bottomHeightProportion)))
+            path.addLine(to: CGPoint(x: drawingRect.minX + drawingRect.width * (1 - bottomProportion) / 2,
+                                     y: drawingRect.maxY))
+            path.addLine(to: CGPoint(x: drawingRect.maxX - drawingRect.width * (1 - bottomProportion) / 2,
+                                     y: drawingRect.maxY))
+            path.addLine(to: CGPoint(x: drawingRect.maxX,
+                                     y: drawingRect.minY + drawingRect.height * (1 - bottomHeightProportion)))
+            path.addLine(to: CGPoint(x: drawingRect.maxX - drawingRect.width * (1 - topProportion) / 2,
+                                  y: drawingRect.minY))
+            path.close()
+            layer.lineWidth = lineWidth
+            switch piece.color {
+            case .blue:
+                layer.fillColor = UIColor(named: "bluePlayerColor")?.cgColor
+                layer.strokeColor = UIColor(named: "bluePlayerColor")?.lighter().cgColor
+            case .white:
+                layer.fillColor = UIColor(named: "whitePlayerColor")?.cgColor
+                layer.strokeColor = UIColor(named: "whitePlayerColor")?.darker().cgColor
+                path.apply(CGAffineTransform(scaleX: 1, y: -1))
+                path.apply(CGAffineTransform(translationX: 0, y: bounds.height))
+            }
+            if isSelected {
+                layer.strokeColor = UIColor.red.cgColor
+            }
+            layer.path = path.cgPath
         }
-        if isSelected {
-            UIColor.systemRed.setStroke()
-        }
-        path.fill()
-        path.stroke()
         
-        let text: String
-        switch piece.type {
-        case .field:
-            return
-        case .empress:
-            text = "E"
-        case .burrow:
-            text = "B"
-        case .rabbit:
-            text = "R"
-        case .hare:
-            text = "H"
-        case .moon:
-            text = "M"
         }
-        let attrString = NSAttributedString(string: text, attributes: [
-            .foregroundColor: textColor,
-            .font: UIFont.boldSystemFont(ofSize: fontSize)
-        ])
-        let textSize = attrString.size()
-        let textRect = CGRect(x: bounds.midX, y: bounds.midY, width: 0, height: 0)
-            .insetBy(dx: -textSize.width / 2, dy: -textSize.height / 2)
-            .applying(CGAffineTransform(translationX: 0, y: textOffset))
-        attrString.draw(in: textRect)
     }
 }
