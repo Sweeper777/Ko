7682597a111ed23843c64d710a9ba1d3e9bb7cae
Mulang Su <sumulang@gmail.com>
2021-10-05 08:41:06 +0100

    tested empress existence rule

--- KoModelMacOSTests/ExistenceRuleTests.swift
+++ KoModelMacOSTests/ExistenceRuleTests.swift
@@ -3,4 +3,25 @@ import XCTest
 
 class ExistenceRuleTests: XCTestCase {
 
+    func testEmpressExistenceRule() {
+        let game = Game()
+        game.board.placePiece(Piece(.blue, .empress), at: .init(9, 9))
+        game.currentTurnNumber = 10
+        game.currentTurn = .white
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = [empressExistenceRule]
+        XCTAssertNil(
+            ruleResolver.resolve(against: .placePiece(.empress, at: .init(10, 10)), game: game)
+        )
+        XCTAssertNil(
+            ruleResolver.resolve(against: .placePiece(.empress, at: .init(9, 10)), game: game)
+        )
+        XCTAssertNotNil(
+            ruleResolver.resolve(against: .placePiece(.empress, at: .init(0, 0)), game: game)
+        )
+        XCTAssertNotNil(
+            ruleResolver.resolve(against: .placePiece(.empress, at: .init(7, 9)), game: game)
+        )
+    }
+    
 }
