b71523e6f77027c4b08a4779d99a81e19fa1486e
Mulang Su <sumulang@gmail.com>
2021-10-05 08:40:35 +0100

    tested move to same square rule

--- KoModelMacOSTests/MiscRuleTests.swift
+++ KoModelMacOSTests/MiscRuleTests.swift
@@ -3,5 +3,16 @@ import XCTest
 
 class MiscRuleTests: XCTestCase {
 
+    func testNoMoveToSameSquareRule() {
+        let game = Game()
+        game.board.placePiece(Piece(.blue, .empress), at: .init(9, 9))
+        game.currentTurnNumber = 10
+        game.currentTurn = .blue
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = [noMoveToSameSquareRule]
+        XCTAssertNil(
+            ruleResolver.resolve(against: .move(from: .init(9, 9), to: .init(9, 9)), game: game)
+        )
+    }
 
 }
