3a8e6a5ceece88dafa50688fc448b4219e2e60c7
Mulang Su <sumulang@gmail.com>
2021-10-05 08:41:22 +0100

    tested empress-moon existence rule

--- KoModelMacOSTests/ExistenceRuleTests.swift
+++ KoModelMacOSTests/ExistenceRuleTests.swift
@@ -24,4 +24,34 @@ class ExistenceRuleTests: XCTestCase {
         )
     }
     
+    func testEmpressMoonExistenceRules() {
+        let game = Game()
+        game.board.placePiece(Piece(.blue, .empress), at: .init(1, 1))
+        game.board.placePiece(Piece(.blue, .moon), at: .init(5, 1))
+        game.board.placePiece(Piece(.white, .empress), at: .init(1, 5))
+        game.board.placePiece(Piece(.white, .moon), at: .init(5, 5))
+        game.currentTurnNumber = 10
+        game.currentTurn = .white
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = [empressMoonExistenceRule, moonEmpressExistenceRule, moonExistenceRule]
+        XCTAssertNil(
+            ruleResolver.resolve(against: .placePiece(.moon, at: .init(5, 2)), game: game)
+        )
+        XCTAssertNil(
+            ruleResolver.resolve(against: .placePiece(.moon, at: .init(6, 2)), game: game)
+        )
+        XCTAssertNil(
+            ruleResolver.resolve(against: .placePiece(.moon, at: .init(0, 1)), game: game)
+        )
+        XCTAssertNotNil(
+            ruleResolver.resolve(against: .placePiece(.moon, at: .init(2, 2)), game: game)
+        )
+        XCTAssertNotNil(
+            ruleResolver.resolve(against: .placePiece(.moon, at: .init(5, 6)), game: game)
+        )
+        XCTAssertNotNil(
+            ruleResolver.resolve(against: .placePiece(.moon, at: .init(1, 6)), game: game)
+        )
+    }
+    
 }
