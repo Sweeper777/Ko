574445e4e76610011369574bee3c0aa4d4f43dc7
Mulang Su <sumulang@gmail.com>
2021-10-06 19:56:50 +0100

    added stubs for testing the other rules

---
+++ KoModelMacOSTests/EmpressCastleTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+@testable import KoModel
+
+class EmpressCastleTests: XCTestCase {
+
+
+}
---
+++ KoModelMacOSTests/MoonTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+@testable import KoModel
+
+class MoonTests: XCTestCase {
+
+
+}
