b66a33871aa5e8b5cb543ebd454c63bac326dbf8
Mulang Su <sumulang@gmail.com>
2021-10-06 19:53:53 +0100

    added setUp in HareTests

--- KoModelMacOSTests/HareTests.swift
+++ KoModelMacOSTests/HareTests.swift
@@ -2,8 +2,9 @@ import XCTest
 @testable import KoModel
 
 class HareTests : XCTestCase {
+    var game: Game!
+    
     func testReachableByHare() {
-        let game = Game()
         var moveResult = MoveResult()
         let rule = HareMovementRule()
         XCTAssertEqual(rule.apply(to: game, move: .move(from: .init(9, 9), to: .init(10, 7)), pendingMoveResult: &moveResult),
@@ -15,6 +16,7 @@ class HareTests : XCTestCase {
         game.board.placePiece(Piece(.blue, .empress), at: .init(10, 8))
         XCTAssertEqual(rule.apply(to: game, move: .move(from: .init(9, 9), to: .init(10, 7)), pendingMoveResult: &moveResult),
                        .compliance)
+        game.board.removePiece(at: .init(9, 8))
         game.board.placePiece(Piece(.blue, .empress), at: .init(9, 8))
         XCTAssertEqual(rule.apply(to: game, move: .move(from: .init(9, 9), to: .init(9, 7)), pendingMoveResult: &moveResult),
                        .violation)
@@ -24,4 +26,22 @@ class HareTests : XCTestCase {
         XCTAssertEqual(rule.apply(to: game, move: .move(from: .init(9, 9), to: .init(9, 8)), pendingMoveResult: &moveResult),
                        .compliance)
     }
+    
+    override func setUp() {
+        game = Game()
+        game.board.placePiece(.init(.blue, .hare), at: .init(9, 9))
+        game.board.placePiece(.init(.white, .field), at: .init(10, 9))
+        game.board.placePiece(.init(.white, .field), at: .init(11, 9))
+        game.board.placePiece(.init(.white, .field), at: .init(12, 9))
+        game.board.placePiece(.init(.white, .field), at: .init(8, 9))
+        game.board.placePiece(.init(.white, .field), at: .init(7, 9))
+        game.board.placePiece(.init(.white, .field), at: .init(6, 9))
+        game.board.placePiece(.init(.white, .field), at: .init(9, 10))
+        game.board.placePiece(.init(.white, .field), at: .init(9, 11))
+        game.board.placePiece(.init(.white, .field), at: .init(9, 12))
+        game.board.placePiece(.init(.white, .field), at: .init(9, 8))
+        game.board.placePiece(.init(.white, .field), at: .init(9, 7))
+        game.board.placePiece(.init(.white, .field), at: .init(9, 6))
+    }
+    
 }
