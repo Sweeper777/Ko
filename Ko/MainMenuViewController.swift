2dc63e7e86550bcafc6c31037732ee1c351aa24a
Mulang Su <sumulang@gmail.com>
2021-10-08 19:52:34 +0100

    added UIColor.desaturated extension

--- Ko/MainMenuViewController.swift
+++ Ko/MainMenuViewController.swift
@@ -9,3 +9,14 @@ class MainMenuViewController : UIViewController {
 
 }
 
+extension UIColor {
+
+    func desaturated() -> UIColor {
+        var hue: CGFloat = 0
+        var saturation: CGFloat = 0
+        var brightness: CGFloat = 0
+        var alpha: CGFloat = 0
+        getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+        return UIColor(hue: hue, saturation: max(0, saturation - 0.2), brightness: brightness, alpha: alpha)
+    }
+}
