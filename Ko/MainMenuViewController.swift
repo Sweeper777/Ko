48f3c4460fa67b73bcc34eb3d88daa36764b5a6e
Mulang Su <sumulang@gmail.com>
2021-10-08 19:52:24 +0100

    added MainMenuViewController.swift

---
+++ Ko/MainMenuViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+import SwiftyButton
+import SwiftyUtils
+
+class MainMenuViewController : UIViewController {
+    @IBOutlet var startButton: PressableButton!
+    @IBOutlet var connectButton: PressableButton!
+    @IBOutlet var helpButton: PressableButton!
+
+}
+
