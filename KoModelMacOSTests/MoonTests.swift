71cf1a5cf511bde6ff743a65e4d994588957b36f
Mulang Su <sumulang@gmail.com>
2021-10-11 18:47:20 +0100

    set up moon tests

--- KoModelMacOSTests/MoonTests.swift
+++ KoModelMacOSTests/MoonTests.swift
@@ -3,5 +3,23 @@ import XCTest
 
 class MoonTests: XCTestCase {
 
-
+    var game: Game!
+    
+    let fieldPositions = [
+        Position(9, 8), Position(10, 8),
+        Position(8, 9), Position(11, 9),
+        Position(8, 10), Position(11, 10),
+        Position(9, 11), Position(10, 11)
+    ]
+    
+    
+    override func setUp() {
+        game = Game()
+        game.currentTurn = .blue
+        game.currentTurnNumber = 10
+        for pos in fieldPositions {
+            game.board.placePiece(Piece(.blue, .field), at: pos)
+        }
+        game.board.placePiece(Piece(.blue, .moon), at: .init(9, 9))
+    }
 }
