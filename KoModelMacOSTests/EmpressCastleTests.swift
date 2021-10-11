6dc0b94b94846317e8eac443fde0f2b235427d8c
Mulang Su <sumulang@gmail.com>
2021-10-11 18:48:15 +0100

    set up empress castle tests

--- KoModelMacOSTests/EmpressCastleTests.swift
+++ KoModelMacOSTests/EmpressCastleTests.swift
@@ -3,5 +3,26 @@ import XCTest
 
 class EmpressCastleTests: XCTestCase {
 
-
+    var game: Game!
+    
+    override func setUp() {
+        game = Game()
+        game.currentTurn = .blue
+        game.currentTurnNumber = 52
+        
+        for x in 2...16 {
+            game.board.placePiece(Piece(.blue, .field), at: .init(x, 2))
+            game.board.placePiece(Piece(.blue, .field), at: .init(x, 15))
+        }
+        for y in 3...14 {
+            game.board.placePiece(Piece(.blue, .field), at: .init(2, y))
+            game.board.placePiece(Piece(.blue, .field), at: .init(16, y))
+        }
+        game.board.placePiece(Piece(.blue, .field), at: .init(3, 3))
+        game.board.placePiece(Piece(.blue, .field), at: .init(4, 3))
+        game.board.placePiece(Piece(.blue, .burrow), at: .init(5, 3))
+        game.board.placePiece(Piece(.blue, .burrow), at: .init(6, 3))
+        game.board.placePiece(Piece(.blue, .burrow), at: .init(7, 3))
+    }
+    
 }
