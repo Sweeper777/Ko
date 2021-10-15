5fb1a6353293d695f98ba4a9c15e8de8b35e9bc1
Mulang Su <sumulang@gmail.com>
2021-10-15 20:51:14 +0100

    set up rabbit tests

--- KoModelMacOSTests/RabbitTests.swift
+++ KoModelMacOSTests/RabbitTests.swift
@@ -3,5 +3,17 @@ import XCTest
 
 class RabbitTests: XCTestCase {
 
-
+    var game: Game!
+    
+    override func setUp() {
+        game = Game()
+        game.currentTurn = .blue
+        game.currentTurnNumber = 10
+        for x in 8...10 {
+            for y in 8...10 {
+                game.board.placePiece(Piece(.white, .field), at: .init(x, y))
+            }
+        }
+    }
+    
 }
