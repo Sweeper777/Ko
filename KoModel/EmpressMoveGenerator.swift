5961b44dd0e19e1cb7c305067258d98d81a7b1ff
Mulang Su <sumulang@gmail.com>
2021-10-16 21:47:13 +0100

    added EmpressMoveGenerator.canMove

--- KoModel/EmpressMoveGenerator.swift
+++ KoModel/EmpressMoveGenerator.swift
@@ -7,4 +7,14 @@ public enum EmpressMoveGenerator {
             neighbours.map { .move(from: position, to: $0) }.filter { ruleResolver.resolve(against: $0, game: game) != nil }
         )
     }
+    
+    public static func canMove(fromStartingPosition position: Position, game: Game) -> Bool {
+        let neighbours = position.eightNeighbours
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = allRules
+        return !neighbours.lazy
+            .map { .move(from: position, to: $0) }
+            .filter { ruleResolver.resolve(against: $0, game: game) != nil }
+            .isEmpty
+    }
 }
