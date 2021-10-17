7b7c117974a86122d9e2e34d56352653166b1c1a
Mulang Su <sumulang@gmail.com>
2021-10-17 09:50:50 +0100

    added HareMoveGenerator.canMove

--- KoModel/HareRules.swift
+++ KoModel/HareRules.swift
@@ -84,6 +84,16 @@ public enum HareMoveGenerator {
             .filter { ruleResolver.resolve(against: $0, game: game) != nil })
     }
     
+    public static func canMove(fromStartingPosition position: Position, game: Game) -> Bool {
+        let candidates = candidatePositions(fromStartingPosition: position, game: game)
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = allRules
+        return !candidates.lazy
+            .map { Move.move(from: position, to: $0) }
+            .filter { ruleResolver.resolve(against: $0, game: game) != nil }
+            .isEmpty
+    }
+    
     public static func candidatePositions(fromStartingPosition position: Position, game: Game) -> Set<Position> {
         var candidatePositions = game.board.positionsReachableByHare(from: position)
         if let positiveXEmpty = (stride(from: position.x, to: GameConstants.boardColumns, by: 1)
