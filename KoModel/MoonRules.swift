3736bece5f78b7a1d7562a477e91d2685a1379d4
Mulang Su <sumulang@gmail.com>
2021-10-17 09:51:00 +0100

    added MoonMoveGenerator.canMove

--- KoModel/MoonRules.swift
+++ KoModel/MoonRules.swift
@@ -65,4 +65,21 @@ public enum MoonMoveGenerator {
             .map { Move.move(from: position, to: $0) }
             .filter { ruleResolver.resolve(against: $0, game: game) != nil })
     }
+    
+    public static func canMove(fromStartingPosition position: Position, game: Game) -> Bool {
+        let allPositionsReachable = game.board.positionsReachableByMoon(from: position)
+        let candidatePositions = allPositionsReachable.lazy.filter { position in
+            position.eightNeighbours.contains {
+                game.board.board[safe: $0] != nil &&
+                    !game.board[$0].isEmpty &&
+                    game.board[$0].top != Piece(game.currentTurn, .moon)
+            }
+        }
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = rulesWithoutMovementRule
+        return !candidatePositions
+            .map { Move.move(from: position, to: $0) }
+            .filter { ruleResolver.resolve(against: $0, game: game) != nil }
+            .isEmpty
+    }
 }
