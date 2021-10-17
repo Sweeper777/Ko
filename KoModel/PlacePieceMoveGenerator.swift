f2ed1748ad85be92ed2c93515bed06ec36bc9544
Mulang Su <sumulang@gmail.com>
2021-10-17 09:49:41 +0100

    added PlacePieceMoveGenerator.canPlacePiece

--- KoModel/PlacePieceMoveGenerator.swift
+++ KoModel/PlacePieceMoveGenerator.swift
@@ -37,4 +37,39 @@ public enum PlacePieceMoveGenerator {
             return Set(empressNeighbours.map { Move.placePiece(piece, at: $0) }.filter { ruleResolver.resolve(against: $0, game: game) != nil })
         }
     }
+    
+    public static func canPlacePiece(_ piece: PieceType, game: Game) -> Bool {
+        if game.currentTurnNumber < 4 && piece == .field {
+            return true
+        }
+        if game.currentTurnNumber == 4 && piece == .empress {
+            let playersFieldPositions = game.board.piecesPositions[Piece(game.currentTurn, .field)] ?? []
+            let playersFieldNeighbours = Set(playersFieldPositions.flatMap { $0.eightNeighbours })
+            let ruleResolver = RuleResolver()
+            ruleResolver.rules = gameBeginningRulesOnly
+            return !playersFieldNeighbours.lazy
+                .map { Move.placePiece(piece, at: $0) }
+                .filter { ruleResolver.resolve(against: $0, game: game) != nil }
+                .isEmpty
+        }
+        guard let empressPosition = game.board.piecesPositions[Piece(game.currentTurn, .empress)]?.first else {
+            return false
+        }
+        let empressNeighbours = empressPosition.eightNeighbours
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = allRules
+        
+        if piece == .burrow {
+            let candidateLocations = Set(empressNeighbours.flatMap { $0.fourNeighbours })
+            return !candidateLocations.lazy
+                .map { Move.placePiece(.burrow, at: $0) }
+                .filter { ruleResolver.resolve(against: $0, game: game) != nil }
+                .isEmpty
+        } else {
+            return !empressNeighbours.lazy
+                .map { Move.placePiece(piece, at: $0) }
+                .filter { ruleResolver.resolve(against: $0, game: game) != nil }
+                .isEmpty
+        }
+    }
 }
