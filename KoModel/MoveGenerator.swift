f33137c104185e410998600c9b42680358203af0
Mulang Su <sumulang@gmail.com>
2021-10-18 19:58:48 +0100

    conformed EmpressMoveGenerator to MoveGenerator

--- KoModel/MoveGenerator.swift
+++ KoModel/MoveGenerator.swift
@@ -4,3 +4,29 @@ public protocol MoveGenerator {
     var pieceType: PieceType { get }
 }
 
+// MARK: Empress Move Generator
+public struct EmpressMoveGenerator: MoveGenerator {
+    public func generateMoves(fromStartingPosition position: Position, game: Game) -> Set<Move> {
+        let neighbours = position.eightNeighbours
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = allRules
+        return Set(
+            neighbours.map { .move(from: position, to: $0) }.filter { ruleResolver.resolve(against: $0, game: game) != nil }
+        )
+    }
+    
+    public func canMove(fromStartingPosition position: Position, game: Game) -> Bool {
+        let neighbours = position.eightNeighbours
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = allRules
+        return !neighbours.lazy
+            .map { .move(from: position, to: $0) }
+            .filter { ruleResolver.resolve(against: $0, game: game) != nil }
+            .isEmpty
+    }
+    
+    public var pieceType: PieceType { .empress }
+    
+    public init() {}
+}
+
