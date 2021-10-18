b05294993516cbd6354e3afe85e3ec870ca3ec3b
Mulang Su <sumulang@gmail.com>
2021-10-18 19:59:03 +0100

    conformed RabbitMoveGenerator to MoveGenerator

--- KoModel/MoveGenerator.swift
+++ KoModel/MoveGenerator.swift
@@ -30,3 +30,70 @@ public struct EmpressMoveGenerator: MoveGenerator {
     public init() {}
 }
 
+// MARK: Rabbit Move Generator
+public struct RabbitMoveGenerator : MoveGenerator {
+    public func generateMoves(fromStartingPosition position: Position, game: Game) -> Set<Move> {
+        let candidates = candidatePositions(fromStartingPosition: position, game: game)
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = allRules
+        return Set(candidates
+            .map { Move.move(from: position, to: $0) }
+            .filter { ruleResolver.resolve(against: $0, game: game) != nil })
+    }
+    
+    public func canMove(fromStartingPosition position: Position, game: Game) -> Bool {
+        let candidates = candidatePositions(fromStartingPosition: position, game: game)
+        let ruleResolver = RuleResolver()
+        ruleResolver.rules = allRules
+        return !candidates.lazy
+            .map { Move.move(from: position, to: $0) }
+            .filter { ruleResolver.resolve(against: $0, game: game) != nil }
+            .isEmpty
+    }
+    
+    private func candidatePositions(fromStartingPosition position: Position, game: Game) -> [Position] {
+        var candidatePositions = [Position]()
+        if let positiveXEmpty = (stride(from: position.x, to: GameConstants.boardColumns, by: 1)
+            .map { Position($0, position.y) }
+            .first { game.board[$0].isEmpty }) {
+            candidatePositions.append(positiveXEmpty)
+        }
+        if let negativeXEmpty = (stride(from: position.x, through: 0, by: -1)
+            .map { Position($0, position.y) }
+            .first { game.board[$0].isEmpty }) {
+            candidatePositions.append(negativeXEmpty)
+        }
+        if let positiveYEmpty = (stride(from: position.y, to: GameConstants.boardRows, by: 1)
+            .map { Position(position.x, $0) }
+            .first { game.board[$0].isEmpty }) {
+            candidatePositions.append(positiveYEmpty)
+        }
+        if let negativeYEmpty = (stride(from: position.y, through: 0, by: -1)
+            .map { Position(position.x, $0) }
+            .first { game.board[$0].isEmpty }) {
+            candidatePositions.append(negativeYEmpty)
+        }
+        if let topLeftEmpty = (sequence(first: position, next: { $0.above().left() })
+            .first { game.board.board[safe: $0]?.isEmpty == true }) {
+            candidatePositions.append(topLeftEmpty)
+        }
+        if let topRightEmpty = (sequence(first: position, next: { $0.above().right() })
+            .first { game.board.board[safe: $0]?.isEmpty == true }) {
+            candidatePositions.append(topRightEmpty)
+        }
+        if let bottomLeftEmpty = (sequence(first: position, next: { $0.below().left() })
+            .first { game.board.board[safe: $0]?.isEmpty == true }) {
+            candidatePositions.append(bottomLeftEmpty)
+        }
+        if let bottomRightEmpty = (sequence(first: position, next: { $0.below().right() })
+            .first { game.board.board[safe: $0]?.isEmpty == true }) {
+            candidatePositions.append(bottomRightEmpty)
+        }
+        return candidatePositions
+    }
+    
+    public var pieceType: PieceType { .rabbit }
+    
+    public init() {}
+}
+
