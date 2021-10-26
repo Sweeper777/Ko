170468e8d1691f5ceebe77e64d3217168bd381f6
Mulang Su <sumulang@gmail.com>
2021-10-26 22:05:00 +0100

    Create AIGameControllerStrategy.swift

---
+++ Ko/AIGameControllerStrategy.swift
@@ -0,0 +1,16 @@
+import UIKit
+import KoModel
+import SCLAlertView
+
+class AIGameControllerStrategy : GameControllerStrategy {
+    private unowned var gameViewController: GameViewController
+    var humanPlayerColor: PlayerColor
+    var currentAI: GameAI?
+
+    init(gameViewController: GameViewController, humanPlayerColor: PlayerColor) {
+        self.gameViewController = gameViewController
+        self.humanPlayerColor = humanPlayerColor
+    }
+    
+}
+
