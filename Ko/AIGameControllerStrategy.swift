7733a292c3a3139c4b88f44e588042abfaed932b
Mulang Su <sumulang@gmail.com>
2021-10-26 22:05:23 +0100

    added tryAIMove

--- Ko/AIGameControllerStrategy.swift
+++ Ko/AIGameControllerStrategy.swift
@@ -12,10 +12,53 @@ class AIGameControllerStrategy : GameControllerStrategy {
         self.humanPlayerColor = humanPlayerColor
     }
     
+    func didRestartGame() {
+        tryAIMove()
+    }
+
+    func didEndAnimatingMoveResult(_ moveResult: MoveResult) {
+        tryAIMove()
+    }
+
+    func makeMenuButtons() -> [UIView]? {
+        nil
+    }
+
+    func willMove(_ move: Move) {
+
+    }
 
     var isAITurn: Bool {
         gameViewController.game.currentTurn != humanPlayerColor
     }
     
+    func tryAIMove() {
+        guard isAITurn else {
+            gameViewController.setUserInteractionEnabled(true)
+            return
+        }
+        gameViewController.setUserInteractionEnabled(false)
+        guard case .notDetermined = gameViewController.game.result else {
+            return
+        }
+        
+        currentAI = GameAI(game: gameViewController.game, myColor: gameViewController.game.currentTurn)
+        currentAI?.getNextMove(on: .global(qos: .userInitiated)) { [weak self] aiMove in
+            DispatchQueue.main.async { [weak self] in
+                guard let moveResult = self?.gameViewController.game.makeMoveUnchecked(aiMove) else {
+                    return
+                }
+                self?.gameViewController.updateViews()
+                self?.gameViewController.boardView.animateMoveResult(moveResult) { [weak self] in
+                    self?.gameViewController.setUserInteractionEnabled(true)
+                    if case .wins(let color) = self?.gameViewController.game.result {
+                        SCLAlertView().showInfo("Game Over!", subTitle: "\(color) wins!")
+                    } else if .draw == self?.gameViewController.game.result {
+                        SCLAlertView().showInfo("Game Over!", subTitle: "It's a draw!")
+                    }
+                }
+            }
+        }
+    }
 }
 
