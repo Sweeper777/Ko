770552245a478db39d003aef9ba8ec6a5378dda8
Mulang Su <sumulang@gmail.com>
2021-10-28 21:51:24 +0100

    conformed StartInfo to NSCoding

--- Ko/InvitationInfo.swift
+++ Ko/InvitationInfo.swift
@@ -16,6 +16,17 @@ final class StartInfo : NSObject, NSCoding {
         self.turns = turns
     }
 
+    func encode(with coder: NSCoder) {
+        coder.encode(turns.mapValues(\.rawValue), forKey: Keys.turns.stringValue)
+    }
+
+    convenience init?(coder: NSCoder) {
+        guard let turns = coder.decodeObject(of: NSDictionary.self, forKey: Keys.turns.stringValue) as? [MCPeerID: Int] else {
+            return nil
+        }
+        self.init(turns: turns.mapValues { PlayerColor(rawValue: $0)! })
+    }
+
     override var description: String {
         "StartInfo(turns: \(turns))"
     }
