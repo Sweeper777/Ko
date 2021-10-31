78c412b700bff757706bd15fa022beb1010e244d
Mulang Su <sumulang@gmail.com>
2021-10-31 07:38:50 +0000

    set selectablePiece in DefaultGameController.didEndAnimatingMoveResult

--- Ko/DefaultGameControllerStrategy.swift
+++ Ko/DefaultGameControllerStrategy.swift
@@ -1,12 +1,20 @@
 import UIKit
 import KoModel
 class DefaultGameControllerStrategy : GameControllerStrategy {
+    private unowned var gameViewController: GameViewController
+
+    init(gameViewController: GameViewController) {
+        self.gameViewController = gameViewController
+    }
+    
     func didRestartGame() {
 
     }
 
     func didEndAnimatingMoveResult(_ moveResult: MoveResult) {
-
+        
+        gameViewController.pieceSelector.selectablePieces =
+            gameViewController.game.allPlaceablePieces()
     }
 
     func makeMenuButtons() -> [UIView]? {
