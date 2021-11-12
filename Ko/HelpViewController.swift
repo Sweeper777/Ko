2585f37078ede1ba5b1bfc509b4651e2b2d9391b
Mulang Su <sumulang@gmail.com>
2021-11-12 22:25:03 +0000

    added helpPageVC(fromHelpPage:)

--- Ko/HelpViewController.swift
+++ Ko/HelpViewController.swift
@@ -5,5 +5,11 @@ class HelpViewController: UIViewController {
     
     @IBOutlet var pageControl: UIPageControl!
     
+    private func helpPageVC(fromHelpPage helpPage: HelpPage) -> UIViewController {
+        let vc = UIStoryboard.main!.instantiateViewController(withIdentifier: "HelpPage") as! HelpPageViewController
+        vc.helpPage = helpPage
+        return vc
+    }
+    
 }
 
